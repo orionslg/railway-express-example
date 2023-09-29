835854e89bd5a2a3bfb6ee1f3635575bd468c7a5
ORION SOLANG <solang.orion@gmail.com>
2023-09-29 15:37:12 +0700

    install basic packages

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
